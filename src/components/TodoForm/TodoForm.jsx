cc4eaf3fda30b901b8b2eb7b76d88dfec147725f
sreey <sreelakshmi12898@gmail.com>
2023-08-11 22:39:27 +0530

    Turn the form component into an overlay

--- src/components/TodoForm/TodoForm.jsx
+++ src/components/TodoForm/TodoForm.jsx
@@ -4,20 +4,23 @@ import styles from './TodoForm.module.css';
 function TodoForm(){
 
     return(
-        <form className={styles.form} >
-            <div>
-                <label htmlFor="">Task:</label>
-                <input type="text" />
-            </div>
-            <div>
-                <label htmlFor="">Status:</label>
-                <select name="" id="">
-                    <option value="">Complete</option>
-                    <option value="">Incomplete</option>
-                </select>
-            </div>
-            <button>Add</button>
-        </form>
+       <div className={styles.container}>
+        <button className={styles.cancel}>x</button>
+            <form className={styles.form} >
+                <div>
+                    <label htmlFor="">Task:</label>
+                    <input type="text" />
+                </div>
+                <div>
+                    <label htmlFor="">Status:</label>
+                    <select name="" id="">
+                        <option value="">Complete</option>
+                        <option value="">Incomplete</option>
+                    </select>
+                </div>
+                <button>Add</button>
+            </form>
+       </div>
     );
 };
 
