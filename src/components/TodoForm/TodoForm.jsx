3f447f7f22c7e9b0e266ec8e2d48c211d579b502
sreey <sreelakshmi12898@gmail.com>
2023-08-11 22:01:49 +0530

    Add form component

--- src/components/TodoForm/TodoForm.jsx
+++ src/components/TodoForm/TodoForm.jsx
@@ -4,7 +4,20 @@ import styles from './TodoForm.module.css';
 function TodoForm(){
 
     return(
-        <form ></form>
+        <form className={styles.form} >
+            <div>
+                <label htmlFor="">Task:</label>
+                <input type="text" />
+            </div>
+            <div>
+                <label htmlFor="">Status:</label>
+                <select name="" id="">
+                    <option value="">Complete</option>
+                    <option value="">Incomplete</option>
+                </select>
+            </div>
+            <button>Add</button>
+        </form>
     );
 };
 
