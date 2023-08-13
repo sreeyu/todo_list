be04425c205fb5327a0f1a3e9d2cd933c0297edf
sreey <sreelakshmi12898@gmail.com>
2023-08-13 19:53:33 +0530

    Get user input

--- src/components/TodoForm/TodoForm.jsx
+++ src/components/TodoForm/TodoForm.jsx
@@ -1,25 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from './TodoForm.module.css';
 
 function TodoForm(props){
+
+    const [todo, setTodo] = useState('');
+
+    const [status, setStatus] = useState('incomplete');
 
     const closeForm = () =>{
         props.onClose(false);
     };
 
+    const getTodo = (event) =>{
+        setTodo(event.target.value);
+    }
+
+    const getStatus = (event) => {
+        setStatus(event.target.value);
+    }
+
+    const getFormValues = (event) => {
+        event.preventDefault();
+        console.log(todo);
+        console.log(status);
+        closeForm();
+    }
+
     return(
        <div className={styles.container}>
         <button className={styles.cancel} onClick={closeForm} >x</button>
-            <form className={styles.form} >
+            <form className={styles.form} onSubmit={getFormValues} >
                 <div>
-                    <label htmlFor="">Task:</label>
-                    <input type="text" />
+                    <label htmlFor="task">Task:</label>
+                    <input type="text" id="task" onChange={getTodo} />
                 </div>
                 <div>
-                    <label htmlFor="">Status:</label>
-                    <select name="" id="">
-                        <option value="">Complete</option>
-                        <option value="">Incomplete</option>
+                    <label htmlFor="status">Status:</label>
+                    <select name="status" id="status" onChange={getStatus}>
+                        <option value="incomplete">Incomplete</option>
+                        <option value="complete">Complete</option>
+                        
                     </select>
                 </div>
                 <button >Add</button>
